345a19d24441ae38f2dcdecb3aa6afed5121527a
batrazjioty <baticpro@gmail.com>
2023-04-20 18:01:43 +0300

    testing simple contract

--- test/index.test.js
+++ test/index.test.js
@@ -8,14 +8,16 @@ const web3 = new Web3(ganache.provider());
 let accounts = [];
 let inbox;
 
+const INIT_MESSAGE = 'hello world!!!';
+
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+    //console.log(accounts);
 
     inbox = await new web3.eth.Contract(contract.Inbox.abi)
         .deploy({
-            data: contract.Inbox.evm.bytecode.object, arguments: ['hello world!!!']
-            })
+            data: contract.Inbox.evm.bytecode.object, arguments: [INIT_MESSAGE]
+        })
         .send({
             from: accounts[0],
             gas: '1000000'
@@ -23,7 +25,23 @@ beforeEach(async () => {
 });
 
 describe('Inbox', () => {
-   it('deploys a contract', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
 
-   });
+    it('correct message', async () => {
+        const message = await inbox.methods.message().call();
+
+        assert.strictEqual(message, INIT_MESSAGE);
+    });
+
+    it('can change the message', async () => {
+        const testingMessage = 'testing message';
+        await inbox.methods.setMessage(testingMessage).send({
+            from: accounts[0],
+        });
+        const message = await inbox.methods.message().call();
+
+        assert.strictEqual(message, testingMessage);
+    });
 });
